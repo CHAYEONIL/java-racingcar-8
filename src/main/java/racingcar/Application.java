65ae50796f804a952f77964036bd41aa1cd849c2
JaeYoon_Cha <49806534+CHAYEONIL@users.noreply.github.com>
2025-10-22 04:14:43 +0900

    feat: Application 전체 게임 흐름 연결

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,32 @@
 package racingcar;
 
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+import racingcar.domain.RacingGame;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<String> carNames = InputView.readCarNames();
+        int rounds = InputView.readRounds();
+
+        Cars cars = new Cars(carNames);
+        RacingGame game = new RacingGame(cars, rounds);
+
+        OutputView.printResultHeader();
+        List<List<Car>> history = game.play();
+        printGameHistory(history);
+
+        List<String> winners = game.getWinners();
+        OutputView.printWinners(winners);
+    }
+
+    private static void printGameHistory(List<List<Car>> history) {
+        for (List<Car> round : history) {
+            OutputView.printRoundResult(round);
+        }
     }
 }
