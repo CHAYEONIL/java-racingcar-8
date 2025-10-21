e37d013e2750134321144487d4ea49d89546a7a4
JaeYoon_Cha <49806534+CHAYEONIL@users.noreply.github.com>
2025-10-22 03:40:35 +0900

    feat: Car 클래스 기본 구조 구현

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,32 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    private static final int FORWARD_THRESHOLD = 4;
+    private static final int RANDOM_MIN = 0;
+    private static final int RANDOM_MAX = 9;
+
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public void move() {
+        int randomValue = Randoms.pickNumberInRange(RANDOM_MIN, RANDOM_MAX);
+        if (randomValue >= FORWARD_THRESHOLD) {
+            position++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
